076407f21c26be12f2c337772625761b0c031b0b
absprague <ashtonsprague@mail.weber.edu>
2016-10-03 10:55:54 -0600

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,83 @@
-public class Deck
-{
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
+
+public class Deck {
+	
+	//ArrayList to hold cards
+	private final ArrayList<Card> deck = new ArrayList<Card>(52);
+	
+	//Constructor
+	public Deck(){
+		
+	}
+	
+	//add cards to deck
+	public void addCard(Card card){
+		deck.add(card);
+	}
+	
+	//see the number of cards in the deck
+	public int deckSize(){
+		return this.deck.size();
+	}
+	
+	//Clears the deck if needed
+	public void clear(){
+		deck.clear();
+	}
+	
+	//Create the deck
+	public void initialize(){            
+            for(Card.Suit suitName : Card.Suit.values()){
+                for(Card.Value num : Card.Value.values()){
+                    Card card = new Card(suitName, num);
+                    this.addCard(card);
+                }
+            }
+	}
+	
+	//Shuffle the deck
+	public void shuffle(){
+		Collections.shuffle(deck);
+	}
+	
+	//return contents of the deck
+	@Override
+	public String toString(){
+		int num = 1;
+		String display = "";
+		for(Card C: deck){
+			display += num + ": " + C.toString() + "\n";
+			num++;
+		}
+		System.out.println(display);
+		System.out.println();
+		return display;
+	}
+	
+	public Card getTopCard() 	
+	{ 		
+		return deck.get(0);
+	}
+	
+	public Card getBottomCard()
+	{
+		return deck.get(deck.size() - 1);
+	}
+	
+	public Card getCard(int index)
+	{
+		return deck.get(index);
+	}
+	
+	public void removeCard()
+	{
+		deck.remove(0);
+	}
+	
 }
