e670b2c10395dc0ce0c99be3e7741eaed7b294cf
Geese <gisela.chodos@gmail.com>
2016-10-04 16:00:42 -0600

    Panel width fix

--- src/Player.java
+++ src/Player.java
@@ -26,21 +26,32 @@ public class Player extends JPanel
 		btn_playTopCard = new JButton("Play Top Card");
 		btn_slap = new JButton("Slap");
 		
-		handPanel = new JPanel();
-		handPanel.setLayout(null);//so that Card objects can be positioned
-		handPanel.setBackground(Color.BLUE);
-		handPanel.setPreferredSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .7)));
-		handPanel.setMinimumSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .7)));
+		createHandPanel();		
+		createControlPanel();
 		
-		controlPanel = new JPanel();
-		controlPanel.setLayout(new FlowLayout());
-		controlPanel.setBackground(Color.GREEN);
-		controlPanel.setPreferredSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .3)));
-		controlPanel.setMinimumSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .3)));
 		controlPanel.add(btn_playTopCard);
 		controlPanel.add(btn_slap);
 		
 		this.add(handPanel);
 		this.add(controlPanel);
+	}
+
+	private void createControlPanel() {
+		controlPanel = new JPanel();
+		controlPanel.setLayout(new FlowLayout());
+		controlPanel.setBackground(Color.GREEN);
+		controlPanel.setPreferredSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .3)));
+		controlPanel.setMinimumSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .3)));
+		controlPanel.setMaximumSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .3)));
+		
+	}
+
+	private void createHandPanel() {
+		handPanel = new JPanel();
+		handPanel.setLayout(null);//so that Card objects can be positioned
+		handPanel.setBackground(Color.BLUE);
+		handPanel.setPreferredSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .7)));
+		handPanel.setMinimumSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .7)));
+		handPanel.setMaximumSize(new Dimension((int)(theBoard.getWidth()*.33), (int)(theBoard.getHeight() * .7)));
 	}
 }
