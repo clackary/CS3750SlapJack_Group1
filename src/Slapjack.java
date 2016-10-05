73a90e0e5c785c9046fb24d95836b042e780133f
absprague <ashtonsprague@mail.weber.edu>
2016-10-05 10:44:00 -0600

    Add driver Slapjack class

---
+++ src/Slapjack.java
@@ -0,0 +1,60 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import javax.swing.WindowConstants;
+
+public class Slapjack extends JFrame{
+
+	public Slapjack(){
+		int screenWidth = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		int screenHeight = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		//takes up whole screen but leaves room at bottom
+		this.setSize(new Dimension(screenWidth, screenHeight - (screenHeight / 20))); 
+		this.setVisible(true);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		createMenu();
+		this.repaint();
+		Board board = new Board();
+		this.add(board);
+		this.pack();//this way the board JPanel shows up
+
+	}
+	
+	private void createMenu(){
+		JMenuBar menuBar = new JMenuBar();
+		JMenu gameMenu = new JMenu("Game");
+		JMenuItem newGame = new JMenuItem("New Game");
+		newGame.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				newGame();
+			}
+		});
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		gameMenu.add(newGame);
+		gameMenu.add(exit);
+		menuBar.add(gameMenu);
+		this.setJMenuBar(menuBar);
+	}
+	
+	private void newGame(){
+		this.dispose();
+		new Slapjack();
+	}
+	
+	public static void main(String[] args){
+		new Slapjack();
+	}
+}
