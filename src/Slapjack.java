c558323275e3b3f97cecfd169035572956dc131a
Geese <gisela.chodos@gmail.com>
2016-10-08 17:53:06 -0600

    Finished Action Key menu options

--- src/Slapjack.java
+++ src/Slapjack.java
@@ -1,5 +1,4 @@
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,38 +6,22 @@ import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JSeparator;
-import javax.swing.KeyStroke;
-import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 public class Slapjack extends JFrame{
 	
 	private boolean soundOn = true;
 	
-	private String playTopKey_p1 = "";
-	private String slapKey_p1 = "";
-	
 	private Board board;
 	private MenuActionListener mal = new MenuActionListener();
 	
 	//using this to turn off the glow after Player collects center pile
 	public static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-	private JPanel keyBindingsPanel;
 	
 	public Slapjack(){
 		int screenWidth = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
@@ -100,12 +83,12 @@ public class Slapjack extends JFrame{
 		JMenu setKeysMenu = new JMenu("Set Action Keys");
 		
 		JMenu setKeysMenu_Player1 = new JMenu("Player 1");
+		JMenu setKeysMenu_Player2 = new JMenu("Player 2");
 		
-		ArrayList<JMenuItem> playTopMenuItems_p2 = new ArrayList<>();
 		
-		ArrayList<JMenuItem> slapMenuItems_p2 = new ArrayList<>();
-		String [] keyChoices_p1 = {"1", "2","3","4","Q","W","E","R","A","S","D","F","Z","X","C","V"};
-		String [] keyChoices_p2 = {"7", "8","9","0","U","I","O","P","J","K","L",";","N","M",",","."};
+		
+		String [] keyChoices_p1 = {"1", "2","3","4","Q","W","E","R","A","S","D","F","Z","X"};
+		String [] keyChoices_p2 = {"7", "8","9","0","U","I","O","P","H","J","K","L","N","M"};
 		
 		JMenu playTop_p1 = new JMenu("Play Top Card");
 		ArrayList<JMenuItem> playTopMenuItems_p1 = new ArrayList<>();
@@ -125,38 +108,30 @@ public class Slapjack extends JFrame{
 			slap_p1.add(slapMenuItems_p1.get(slapMenuItems_p1.size()-1));
 		}
 		
-		
-	/*	for (String choice : keyChoices_p1){
-			playTopMenuItems_p1.add(new JMenuItem(choice));
-			playTopMenuItems_p1.get(playTopMenuItems_p1.size()-1).setName("ptp1");
-			playTopMenuItems_p1.get(playTopMenuItems_p1.size()-1).addActionListener(mal);
-			playTop_p1.add(playTopMenuItems_p1.get(playTopMenuItems_p1.size()-1));
+		JMenu playTop_p2 = new JMenu("Play Top Card");
+		ArrayList<JMenuItem> playTopMenuItems_p2 = new ArrayList<>();
+		for (String choice : keyChoices_p2){
+			playTopMenuItems_p2.add(new JMenuItem(choice));
+			playTopMenuItems_p2.get(playTopMenuItems_p2.size()-1).setName("ptp2");
+			playTopMenuItems_p2.get(playTopMenuItems_p2.size()-1).addActionListener(mal);
+			playTop_p2.add(playTopMenuItems_p2.get(playTopMenuItems_p2.size()-1));
 		}
-		for (String choice : keyChoices_p1){
-			playTopMenuItems_p1.add(new JMenuItem(choice));
-			playTopMenuItems_p1.get(playTopMenuItems_p1.size()-1).setName("ptp1");
-			playTopMenuItems_p1.get(playTopMenuItems_p1.size()-1).addActionListener(mal);
-			playTop_p1.add(playTopMenuItems_p1.get(playTopMenuItems_p1.size()-1));
-		}*/
 		
-		
-		/*JMenuItem slap_p1_1 = new JMenuItem("1");
-		JMenuItem slap_p1_2 = new JMenuItem("2");
-		JMenuItem slap_p1_3 = new JMenuItem("W");
-		JMenuItem slap_p1_4 = new JMenuItem("S");
-		JMenuItem slap_p1_5 = new JMenuItem("X");
-		
-		
-		
-		slap_p1.add(slap_p1_1);
-		slap_p1.add(slap_p1_2);
-		slap_p1.add(slap_p1_3);
-		slap_p1.add(slap_p1_4);
-		slap_p1.add(slap_p1_5);*/
+		JMenu slap_p2 = new JMenu("Slap");
+		ArrayList<JMenuItem> slapMenuItems_p2 = new ArrayList<>();
+		for (String choice : keyChoices_p2){
+			slapMenuItems_p2.add(new JMenuItem(choice));
+			slapMenuItems_p2.get(slapMenuItems_p2.size()-1).setName("slp2");
+			slapMenuItems_p2.get(slapMenuItems_p2.size()-1).addActionListener(mal);
+			slap_p2.add(slapMenuItems_p2.get(slapMenuItems_p2.size()-1));
+		}
 		
 		setKeysMenu_Player1.add(playTop_p1);
 		setKeysMenu_Player1.add(slap_p1);
+		setKeysMenu_Player2.add(playTop_p2);
+		setKeysMenu_Player2.add(slap_p2);
 		setKeysMenu.add(setKeysMenu_Player1);
+		setKeysMenu.add(setKeysMenu_Player2);
 		optionsMenu.add(setKeysMenu);
 		
 		
@@ -210,58 +185,3 @@ public class Slapjack extends JFrame{
 	}
 }
 
-
-
-
-
-
-
-
-/*	private void createKeyBindingsPanel(){
-		String[] playTop_choices_1 = {"A","S","D"};
-		String[] playTop_choices_2 = {"J","K","L"};
-		String[] slap_choices_1 = {"A","S","D"};
-		String[] slap_choices_2 = {"J","K","L"};
-		
-		JComboBox playTop_comboBox_1 = new JComboBox(playTop_choices_1);
-		JComboBox playTop_comboBox_2 = new JComboBox(playTop_choices_2);
-		JComboBox slap_comboBox_1 = new JComboBox(slap_choices_1);
-		JComboBox slap_comboBox_2 = new JComboBox(slap_choices_2);
-		
-		keyBindingsPanel = new JPanel();
-		keyBindingsPanel.setLayout(new BoxLayout(keyBindingsPanel, BoxLayout.X_AXIS));
-		JPanel player1KeysPanel = new JPanel();
-		JPanel player2KeysPanel = new JPanel();
-		player1KeysPanel.setLayout(new GridLayout(3,2));
-		player2KeysPanel.setLayout(new GridLayout(3,2));
-		
-		JLabel plyr_1 = new JLabel("Player 1");
-		plyr_1.setHorizontalAlignment(SwingConstants.CENTER);
-		player1KeysPanel.add(plyr_1);
-		player1KeysPanel.add(Box.createRigidArea(new Dimension(1,1)));//placeholder
-		JLabel ptc_1 = new JLabel("Play Top Card:");
-		ptc_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		JLabel slap_1 = new JLabel("Slap:");
-		slap_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		player1KeysPanel.add(ptc_1);
-		player1KeysPanel.add(playTop_comboBox_1);
-		player1KeysPanel.add(slap_1);
-		
-		JLabel plyr_2 = new JLabel("Player 2");
-		plyr_2.setHorizontalAlignment(SwingConstants.CENTER);
-		player2KeysPanel.add(plyr_2);
-		player2KeysPanel.add(Box.createRigidArea(new Dimension(1,1)));//placeholder
-		JLabel ptc_2 = new JLabel("Play Top Card:");
-		ptc_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		JLabel slap_2 = new JLabel("Slap:");
-		slap_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		player2KeysPanel.add(ptc_2);
-		player2KeysPanel.add(Box.createRigidArea(new Dimension(1,1)));//placeholder for where dropdown will go
-		player2KeysPanel.add(slap_2);
-		
-		
-		
-		keyBindingsPanel.add(player1KeysPanel);
-		keyBindingsPanel.add(new JSeparator(SwingConstants.VERTICAL));
-		keyBindingsPanel.add(player2KeysPanel);
-	}*/
